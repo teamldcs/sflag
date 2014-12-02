f2185dba5a360160a24912a59adb44025f21dd08
Dennis Francis <team@ldcs.co.in>
2014-12-02 06:30:17 +0530

    updated toplevel comment in sflag.go

--- sflag.go
+++ sflag.go
@@ -1,6 +1,6 @@
-// Package sflag is a commandline arguments parser using struct syntax
-// At time of writing, sflag is the only known flags package variant that is 100% DRY, free of fugly pointer syntax and uses clean struct syntax.
+// Package sflag is the only known flags package variant, at the time of writing, that is 100% DRY, free of fugly pointer syntax and uses clean struct syntax.
 // Implementation makes use of reflection and struct tags.
+//
 // BUG() Presence of a boolean flag requires that there be no STANDALONE true or false parameters, use "--Foo=true" syntax instead of "--Foo true".
 package sflag
 
